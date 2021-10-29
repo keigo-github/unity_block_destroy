9b71503e75e7c79ce4d506fcebea03b6b8808d03
keigo-github <85817145+keigo-github@users.noreply.github.com>
2021-10-29 18:53:10 +0900

    Add files via upload

---
+++ GameManager.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+
+    public Block[] blocks;
+    public GameObject gameOverUI;
+    public GameObject gameClearUI;
+
+    private bool isGameClear = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isGameClear != true)
+        {
+            if (DestroyAllBlocks())
+            {
+                //ゲームクリア
+                Debug.Log("ゲームクリア");
+                gameClearUI.SetActive(true);
+                isGameClear = true;
+            }
+        }
+    }
+
+    //ブロック全部消えた？    
+    private bool DestroyAllBlocks()
+    {
+        foreach( Block b in blocks)
+        {
+            if( b != null)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void GameOver()
+    {
+        Debug.Log("ゲームオーバー");
+        gameOverUI.SetActive(true);
+    }
+
+    public void GameRetry()
+    {
+        SceneManager.LoadScene("game");
+    }
+}
